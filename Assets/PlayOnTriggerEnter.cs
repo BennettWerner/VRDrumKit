9a1ce48b33fe546006f20ddbafa1ec1b6022500f
Bennett Werner <bennettjwerner@gmail.com>
2019-06-18 23:13:39 -0700

    drumz are cool

---
+++ Assets/PlayOnTriggerEnter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnTriggerEnter : MonoBehaviour
+{
+    AudioSource thisDrumAudioSource;
+    public AudioClip thisDrumClip;
+    ParticleSystem hitPFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisDrumAudioSource = gameObject.GetComponent<AudioSource>();
+        hitPFX = GetComponentInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (!thisDrumAudioSource.isPlaying)
+        //{
+        thisDrumAudioSource.PlayOneShot(thisDrumClip);
+        hitPFX.Play();
+        Debug.Log("hit");
+        //}
+    }
+}
